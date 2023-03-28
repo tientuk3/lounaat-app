f124bd1df6dab0066afa73ccf1aedabf9951959b
Tuukka <mail@tuukkaorm.io>
2023-03-29 02:59:56 +0300

    UI improvements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import { useState, useEffect } from 'react' 
 import axios from 'axios'
-import Card from 'react-bootstrap/Card'
-import ListGroup from 'react-bootstrap/ListGroup';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import { Card, CardContent, CardActions, Typography, Box, Grid, CardHeader, List, ListItem, ListSubheader, ListItemText } from '@mui/material';
+import { Container } from '@mui/material'
 
-const API_URL = 'https://api.tuk3diagnostics.com/lounaat/'
+const API_URL = 'https://api.tuk3diagnostics.com/lounaat'
 
 const App = () => {
   const [restaurants, setRestaurants] = useState([])
@@ -25,26 +20,28 @@ const App = () => {
 
   return (
     <>
-    <Container fluid>
-      <Row>
-      {restaurants.map((r, i) => (
-          <Col xs="3">
-            <Card text='light'
-                className="dark-card"
-                key={i}
-            >
-              <Card.Body>
-                <Card.Title>{r.name}</Card.Title>
-                <ListGroup variant="flush">
-                  {r.dishes.map((d) => (
-                    <ListGroup.Item className="dark-card">{d}</ListGroup.Item>
-                  ))}
-                </ListGroup>
-              </Card.Body>
+    <Container maxWidth='false'>
+      <Grid container spacing={2}>
+        {restaurants.map((r, i) => (
+          <Grid item>
+            <Card variant="elevation" raised='true' sx={{ maxWidth: 400, borderRadius: 3 }}>
+              <CardContent>
+                <CardHeader align='center' title={r.name} />
+                <Typography align='center' sx={{ fontStyle: 'italic' }}>{r.address}</Typography>
+                <Typography align='center' sx={{ fontStyle: 'italic' }}>Etäisyys {r.distance}</Typography>
+                  <List sx = {{listStyleType: 'disc', pl: 2, }}>
+                    {r.dishes.map((d, i) => (
+                      <ListItem key={i} sx={{ display: 'list-item' }}>
+                        <ListItemText primary={d} />
+                      </ListItem>
+                    ))}
+                  </List>
+              </CardContent>
             </Card>
-          </Col>
-          ))}
-      </Row>
+          </Grid>
+        ))}
+      </Grid>
+      
     </Container>
     
     </>
